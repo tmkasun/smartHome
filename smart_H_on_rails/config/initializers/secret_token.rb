02c8ea0667766fa6d64ed59cab03e5c39a9404fe
Kasun Thennakoon <tmkasun@gmail.com>
2013-10-09 17:50:28 +0530

    moved from PHP to Rails :)

---
+++ smart_H_on_rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmartHOnRails::Application.config.secret_key_base = '91e2dbcd467149bd49229dfb27467f2c11178a0dd70232f6cf98b73ba9df7ede938805f1c00ab3f6fefb83f1ebe4e980b0e7eec18e452b1eacd89064f7709a12'
---
+++ smart_H_on_rails/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmartHOnRails::Application.config.session_store :cookie_store, key: '_smart_H_on_rails_session'
